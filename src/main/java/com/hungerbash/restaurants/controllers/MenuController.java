e1e82f33d2be06aa4896981b33d5a286b63d7543
gagandeepghai <gagdeep@gmail.com>
2018-02-19 16:59:52 -0500

    Update MenuController.java

--- src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
+++ src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
@@ -46,7 +46,7 @@ public class MenuController {
                     + resourceUrl + " PRC: " +podRoutingCookie);
 	HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_XML);
-	return new ResponseEntity<String>("<sitemapindex><sitemap><loc>https://recruiting.adp.com/rm/public/third-party-integration/google/sitemap?s=srccsh&amp;i=001&amp;prc=RMPOD1&samp;i=1</loc><lastmod>2018-02-19T05:01:10-05:00</lastmod></sitemap></sitemapindex>", headers, HttpStatus.OK);
+	return new ResponseEntity<String>("<sitemapindex><sitemap><loc>https://recruiting.adp.com/rm/public/third-party-integration/google/sitemap?s=srccsh&amp;i=001&amp;prc=RMPOD1&amp;si=1</loc><lastmod>2018-02-19T05:01:10-05:00</lastmod></sitemap></sitemapindex>", headers, HttpStatus.OK);
     
     }
 	
