1137f10f3b3ed1f14270e0781814222e761d9d1e
Ghai <ghaig@roselcdv00029rs.es.ad.adp.com>
2018-03-21 10:43:05 -0400

    Google verify test

--- src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
+++ src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
@@ -99,8 +99,7 @@ public class MenuController {
     @GetMapping("/google6a32c23c943668d8.html")
     public void google6a32c23c943668d8(HttpServletResponse response) throws ClientProtocolException, IOException {
     		ClassLoader classLoader = MenuController.class.getClassLoader();
-    		File file = new File(classLoader.getResource("google6a32c23c943668d8.html").getFile());
-    		org.apache.commons.io.IOUtils.copy(new FileInputStream(file), response.getOutputStream());;
+    		org.apache.commons.io.IOUtils.copy(classLoader.getResourceAsStream("google6a32c23c943668d8.html"), response.getOutputStream());;
     }
     
     @GetMapping("/categories/{id}")
