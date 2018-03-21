af5d9942673a880d97c5c75eac793614060f909b
Ghai <ghaig@roselcdv00029rs.es.ad.adp.com>
2018-03-21 14:08:49 -0400

    JSON LD

--- src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
+++ src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
@@ -1,11 +1,12 @@
 package com.hungerbash.restaurants.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.BadRequestException;
-import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -16,7 +17,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 //Spring Imports
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -74,7 +77,7 @@ public class MenuController {
     }
     
     @GetMapping("/jp")
-    public Response testJobPosting(@RequestParam("r") String reqId, 
+    public ResponseEntity<?> testJobPosting(@RequestParam("r") String reqId, 
             @RequestParam("c") Long clientId,
             @RequestParam(value = "u") String url) throws ClientProtocolException, IOException {
     		System.out.println("Sitemap call for c: " + clientId + " r: " + reqId + " u: " + url);
@@ -88,10 +91,13 @@ public class MenuController {
         HttpClient httpClientInstance = HttpClientBuilder.create().useSystemProperties().setDefaultRequestConfig(config).build();
         HttpResponse restResponse = httpClientInstance.execute(new HttpGet(baseUrl));
         
-        String body = IOUtils.toString(restResponse.getEntity().getContent(), encoding);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         
-        return Response.status(Response.Status.OK).entity(body)
-        		.type("application/ld+json").build();
+        String body = IOUtils.toString(restResponse.getEntity().getContent(), encoding);
+        body.replace("Context", "context");
+        
+        return new ResponseEntity<Object>(body, headers, HttpStatus.OK);
     }
 	
     @GetMapping("/google6a32c23c943668d8.html")
