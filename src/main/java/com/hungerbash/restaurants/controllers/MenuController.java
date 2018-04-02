3b9c7c6ff3a381c83cdd2cc014438dd4fe964364
Ghai <ghaig@roselcdv00029rs.es.ad.adp.com>
2018-04-02 10:43:03 -0400

    Dummy data

--- src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
+++ src/main/java/com/hungerbash/restaurants/controllers/MenuController.java
@@ -111,7 +111,7 @@ public class MenuController {
         HttpResponse restResponse = httpClientInstance.execute(getRequest);
         
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_TYPE, "application/ld+json");
+//        headers.set(HttpHeaders.CONTENT_TYPE, "application/ld+json");
         
         String body = IOUtils.toString(restResponse.getEntity().getContent(), encoding);
 //        String responseStr = "<!DOCTYPE html><html><head></head><body><script type=\"application/ld+json\">" + body + "</script></body></html>";
